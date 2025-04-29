541f505baf0e27acb64ab90c945132d0bc20cd6f
Mohammad Mahdi <mohammadmahdikeshavarz1382@gmail.com>
2025-04-29 20:50:21 +0330

    style: format Solidity code using forge fmt
    Formatted code for consistency and readability

--- src/MultiSigWallet.sol
+++ src/MultiSigWallet.sol
@@ -2,21 +2,17 @@
 pragma solidity ^0.8.22;
 
 contract MultiSigWallet {
-    
     address[] private owners;
     uint8 public immutable requireConfirmations;
-    
-    mapping(address => bool) public isOwner;
-    mapping(address => mapping (uint256 => bool)) public isConfirmed;
 
-    constructor(address [] memory _owners, uint8 _requireConfirmations) {
+    mapping(address => bool) public isOwner;
+    mapping(address => mapping(uint256 => bool)) public isConfirmed;
+
+    constructor(address[] memory _owners, uint8 _requireConfirmations) {
         require(_owners.length != 0, "Owners list can't be empty!");
-        require(
-            _owners.length >= _requireConfirmations,
-            "Confirmations can't be greater than number of owners"
-        );
-        
-        for (uint i = 0; i < _owners.length;) {
+        require(_owners.length >= _requireConfirmations, "Confirmations can't be greater than number of owners");
+
+        for (uint256 i = 0; i < _owners.length;) {
             address owner = _owners[i];
             require(owner != address(0), "Owner can't be 0 address");
             require(!isOwner[owner], "Duplicate Owner not accepted");
@@ -24,12 +20,14 @@ contract MultiSigWallet {
             isOwner[owner] = true;
             owners.push(owner);
 
-            unchecked { i++; }
+            unchecked {
+                i++;
+            }
         }
         requireConfirmations = _requireConfirmations;
     }
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(isOwner[msg.sender], "Not an owner!");
         _;
     }
@@ -44,22 +42,17 @@ contract MultiSigWallet {
 
     Transaction[] public transactions;
 
-    function numOwners() public view returns(uint256) {
+    function numOwners() public view returns (uint256) {
         return owners.length;
     }
 
-    function getOwners() public view returns(address[] memory) {
+    function getOwners() public view returns (address[] memory) {
         return owners;
     }
 
     function setTransaction(address _to, uint256 _value, bytes memory _data) external onlyOwner {
-        Transaction memory newTransaction = Transaction ({
-            to: _to,
-            value: _value,
-            data: _data,
-            executed: false,
-            numConfirmations: 0
-        });
+        Transaction memory newTransaction =
+            Transaction({to: _to, value: _value, data: _data, executed: false, numConfirmations: 0});
         transactions.push(newTransaction);
     }
 
@@ -69,7 +62,6 @@ contract MultiSigWallet {
         require(isConfirmed[msg.sender][_txIndex] == false, "You signed this TX before!");
 
         transactions[_txIndex].numConfirmations += 1;
-        isConfirmed[msg.sender][_txIndex] = true;   
+        isConfirmed[msg.sender][_txIndex] = true;
     }
-
 }
--- test/MultiSigWallet.t.sol
+++ test/MultiSigWallet.t.sol
@@ -47,7 +47,6 @@ contract MultiSigWalletTest is Test {
         owners.push(address(0x1));
         vm.expectRevert("Duplicate Owner not accepted");
         multiSigWallet = new MultiSigWallet(owners, requireConfirmations);
-
     }
 
     function testRevert_ownercannotBeZeroAddress() public {
@@ -57,7 +56,7 @@ contract MultiSigWalletTest is Test {
     }
 
     function testRevert_NonOwnerCannotCallSetTransaction() public {
-        address nonOwner = address (0xDe);
+        address nonOwner = address(0xDe);
         vm.label(nonOwner, "NonOwner");
 
         vm.prank(nonOwner);
@@ -75,81 +74,79 @@ contract MultiSigWalletTest is Test {
         multiSigWallet.setTransaction(target, value, data);
 
         // Validate state
-        (address to, uint256 txValue, bytes memory txData, bool executed, uint256 numConfirmations) = multiSigWallet.transactions(0);
+        (address to, uint256 txValue, bytes memory txData, bool executed, uint256 numConfirmations) =
+            multiSigWallet.transactions(0);
 
         assertEq(to, target, "Target address mismatch");
         assertEq(txValue, value, "Transaction value mismatch");
         assertEq(txData, data, "Transaction data mismatch");
         assertEq(executed, false, "Transaction should not be executed yet");
         assertEq(numConfirmations, 0, "Transaction should start with 0 confimations");
-
     }
 
-        function testRevert_ANonExistTxCannotBeSign() public {
-            uint256 txIndex = 1;
-            address owner = owners[0];
-            vm.label(owner, "Owner");
+    function testRevert_ANonExistTxCannotBeSign() public {
+        uint256 txIndex = 1;
+        address owner = owners[0];
+        vm.label(owner, "Owner");
 
-            vm.prank(owner);
-            vm.expectRevert("There is no such TX!");
-            multiSigWallet.signTransaction(txIndex);
-        }
+        vm.prank(owner);
+        vm.expectRevert("There is no such TX!");
+        multiSigWallet.signTransaction(txIndex);
+    }
 
-        // After implementig executeTransaction() function, we can rewrite this test =]
-        function blob_testRevert_AnExecutedTxCannotBeSign() public {
-            // Simulate a transaction
-            address owner = owners[0];
-            vm.prank(owner);
-            multiSigWallet.setTransaction(address(0x1234), 1 wei, "");
-            uint256 txIndex = 0;
+    // After implementig executeTransaction() function, we can rewrite this test =]
+    function blob_testRevert_AnExecutedTxCannotBeSign() public {
+        // Simulate a transaction
+        address owner = owners[0];
+        vm.prank(owner);
+        multiSigWallet.setTransaction(address(0x1234), 1 wei, "");
+        uint256 txIndex = 0;
 
-            vm.prank(owner);
-            // multiSigWallet.executeTransaction(txIndex);
+        vm.prank(owner);
+        // multiSigWallet.executeTransaction(txIndex);
 
-            vm.label(owner, "Owner");
+        vm.label(owner, "Owner");
 
-            vm.prank(owner);
-            vm.expectRevert("This TX has been executed!");
-            multiSigWallet.signTransaction(txIndex);
-        }
+        vm.prank(owner);
+        vm.expectRevert("This TX has been executed!");
+        multiSigWallet.signTransaction(txIndex);
+    }
 
+    function testRevert_ownerCannotSignATxMoreThanOnce() public {
+        address owner1 = owners[0];
+        address owner2 = owners[1];
+        uint256 txIndex;
 
-        function testRevert_ownerCannotSignATxMoreThanOnce() public {
-            address owner1 = owners[0];
-            address owner2 = owners[1];
-            uint256 txIndex;
+        // Initial a transaction by one of the owners
+        vm.prank(owner1);
+        multiSigWallet.setTransaction(address(0x1234), 1 wei, "");
+        txIndex = 0;
 
-            // Initial a transaction by one of the owners
-            vm.prank(owner1);
-            multiSigWallet.setTransaction(address(0x1234), 1 wei, "");
-            txIndex = 0;
-            
-            // Sign the transaction by one of the owners
-            vm.prank(owner2);
-            multiSigWallet.signTransaction(txIndex);
+        // Sign the transaction by one of the owners
+        vm.prank(owner2);
+        multiSigWallet.signTransaction(txIndex);
 
-            // Sign the EXACT transaction again
-            vm.prank(owner2);
-            vm.expectRevert("You signed this TX before!");
-            multiSigWallet.signTransaction(txIndex);
+        // Sign the EXACT transaction again
+        vm.prank(owner2);
+        vm.expectRevert("You signed this TX before!");
+        multiSigWallet.signTransaction(txIndex);
+    }
 
-        }
+    function test_signTransactionByOwdner() public {
+        address owner = owners[0];
+        uint256 txIndex = 0;
 
-        function test_signTransactionByOwdner() public {
-            address owner = owners[0];
-            uint256 txIndex = 0;
+        vm.prank(owner);
+        multiSigWallet.setTransaction(address(0x1234), 1 wei, "");
 
-            vm.prank(owner);
-            multiSigWallet.setTransaction(address(0x1234), 1 wei, "");
+        vm.prank(owner);
+        multiSigWallet.signTransaction(txIndex);
 
-            vm.prank(owner);
-            multiSigWallet.signTransaction(txIndex);
+        (,,,, uint256 numConfirmations) = multiSigWallet.transactions(0);
 
-            (, , , , uint256 numConfirmations) = multiSigWallet.transactions(0);
+        assertEq(numConfirmations, 1);
 
-            assertEq(numConfirmations, 1);
-            
-            bool isConfirmed = multiSigWallet.isConfirmed(owner, txIndex);
-            assertEq(isConfirmed, true);
-        }
+        bool isConfirmed = multiSigWallet.isConfirmed(owner, txIndex);
+        assertEq(isConfirmed, true);
+    }
 }
