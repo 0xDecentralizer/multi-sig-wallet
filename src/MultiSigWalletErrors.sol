2819873c6002dbd1bbd69bb8c2a46323866927c3
Mohammad Mahdi <mohammadmahdikeshavarz1382@gmail.com>
2025-05-16 23:06:05 +0330

    feat: add error declarations for MultiSigWallet

---
+++ src/MultiSigWalletErrors.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+// ============ Errors ============
+error MSW_NotOwner();
+error MSW_OldOwnerInvalid();
+error MSW_TxDoesNotExist();
+error MSW_TxAlreadyExecuted();
+error MSW_TxAlreadySigned();
+error MSW_TxNotSigned();
+error MSW_NotEnoughConfirmations();
+error MSW_InsufficientBalance();
+error MSW_TransactionFailed();
+error MSW_DuplicateOwner();
+error MSW_InvalidOwnerAddress();
+error MSW_EmptyOwnersList();
+error MSW_ConfirmationsExceedOwnersCount();
+error MSW_InvalidRequireConfirmations();
+error MSW_InvalidFunctionSelector();
+error MSW_TransactionExpired();
